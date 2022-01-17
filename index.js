dfeb9a6955761adfc1e9bb85ddd1473df515ebd4
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-01-17 16:55:37 +0530

    added home eouter and controller, changed port

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 8000;
+
+//use express router
+app.use('/',require('./routes'));
 
 
+//run server on port
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
