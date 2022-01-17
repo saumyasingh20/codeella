f4a07db66185eb9a1d7fca4a29e268c4086a7edb
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-01-17 17:33:59 +0530

    added router and controller for users/profile

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,11 @@ const homeController =require('../controllers/home_controller');
 
 console.log("router loaded");
 
+//requests to /'' are sent to their respective controllers
 router.get('/',homeController.home);
+
+router.use('/users',require('./users'));
+
+//router.use('/routerName',require('./routerfile));
+
 module.exports = router;
