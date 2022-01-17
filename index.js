af9440ed3e86a9720c418973d24fa1ab8952c7d0
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-01-17 15:01:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server : ${err}`);
+    }
+
+    console.log(`Server is running on port : ${port}`);
+});
