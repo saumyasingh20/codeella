fdfd86a00080d124b54f5f19f9d4bac54d3cd8f2
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-01-18 21:05:37 +0530

    set up assets directory, static layout and files for pages

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 8002;
-const expressLayouts = require ('express-ejs-layouts');
 
+const expressLayouts = require ('express-ejs-layouts');
+//use the express ejs layouts library
 app.use(expressLayouts);
+
+//use the static files
+
+app.use(express.static('./assets'));
 
 //use express router
 app.use('/',require('./routes'));
 
+//extract styles and scripts from the layout
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 //setting the view engine as ejs and views folder
 app.set('view engine','ejs');
