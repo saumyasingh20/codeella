3e08eb0a7eacc61982940152a9a58b453d8f4eb4
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-01-21 13:50:32 +0530

    added sign in functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,21 @@
 //importing the user schema from models/user
 const User = require('../models/user');
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title:"User Profile"
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(err){
+                console.log('invalid user id');
+            }
+            if(user){
+                return res.render('user_profile',{
+                    title:`${user.first_name}'s Profile`,
+                    user: user
+                })
+            }
+        })
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 };
 
 module.exports.posts = function(req,res){
@@ -59,5 +71,31 @@ module.exports.create =function(req,res){
 }
 //sign in and create a ssession for the user
 module.exports.createSession = function(req,res){
-     //to do later
+    //steps to authenticate
+    //find the user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('error in finding user in sign in process');
+            return;
+        }
+
+   
+    //handle user found
+        if(user){
+             //handle password which don't match
+            if(user.password != req.body.password){
+                console.log('incorrect password');
+                return res.redirect('back');
+            }
+             //handle session creation
+             res.cookie('user_id',user.id);
+             
+             return res.redirect('/users/profile');
+
+        }else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    });
+
 }
