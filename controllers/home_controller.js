fcc5ad47d9cb8c5ae06058be9d88c687e102ba22
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-01-22 15:49:26 +0530

    displayed posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,12 @@
+const Post = require('../models/post');
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title:"Home"
-    });
 
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: "Codeella | Home",
+            posts:posts
+        });
+    })
 }
+
+
