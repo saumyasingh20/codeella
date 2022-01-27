de95415b375dd90052bcd9ffbc488f43ffddf320
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-01-27 22:08:14 +0530

    optimized posts_controller

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,31 +1,37 @@
 //importing post schema
 const Post = require('../models/post');
 const Comment = require('../models/comment');
-module.exports.create = function(req,res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    },function(err,post){
-        if(err){console.log('error in creating a post'); return;}
-
+module.exports.create = async function(req,res){
+    try{
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
         return res.redirect('back');
+    }catch(err){
+        console.log('Error',err);
+        return;
     }
-    
-    )
+   
 }
 
-module.exports.destroy = function(req,res){
-    Post.findById(req.params.id,function(err,post){
-
+module.exports.destroy = async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
         //make a check if the person is authorized to delete the post
         //.id means converting the object id into string
         if(post.user==req.user.id){
             post.remove();  
-            Comment.deleteMany({post:req.params.id},function(err){
-                return res.redirect('back');
-            });
-        }else{
+                
+            await Comment.deleteMany({post:req.params.id});
             return res.redirect('back');
         }
-    })
+        else {
+            return res.redirect('back');
+        }
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
+    
 }
