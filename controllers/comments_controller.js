a8bf1e8bf0f8081455c0a0029b4bb6149030ef98
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-01-27 22:18:20 +0530

    optimized comments_controller

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,39 +1,44 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req,res){
-    Post.findById(req.body.post,function(err,post){
-
+module.exports.create = async function(req,res){
+    try{
+        let post = await Post.findById(req.body.post);
         if(post){
-            Comment.create({
+            let comment = await Comment.create({
                 content:req.body.content,
                 post :req.body.post,
                 user:req.user._id
-            },function(err,comment){
-                //handle error
-                post.comments.push(comment);
-                //use save after every update to store it into the db
-                post.save();
+            });
+            post.comments.push(comment);
+            //use save after every update to store it into the db
+            post.save();
 
-                res.redirect('/');
-            })
+            res.redirect('/');
         }
-    })
+
+    } catch(err){
+        console.log('Error',err);
+    }
+          
 }
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
+module.exports.destroy = async function(req, res){
+    try{
+        let comment = await Comment.findById(req.params.id);
         // if ((comment.user == req.user.id) || (comment.post == req.user.id)){
         if (comment.user == req.user.id){
             let postId = comment.post;
 
             comment.remove();
 
-            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post){
-                return res.redirect('back');
-            })
+        let post = await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+        return res.redirect('back');
+           
         }else{
             return res.redirect('back');
         }
-    });
+    }catch(err){
+        console.log('Error',err);
+    }
 }
