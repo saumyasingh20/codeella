88eb3a113d44cef533dacada495329538915a70a
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-01-27 21:56:29 +0530

    optimized code of home_controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,26 +1,35 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
 
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user',
-        }
-    })
-    .exec(function(err,posts){
-        //to find all the users
-        User.find({},function(err,users){
-            return res.render('home',{
-                title: "Codeella | Home",
-                posts:posts,
-                all_users:users
-            });
+    try{
+         //populate the user of each post
+        let posts = await Post.find({})
+         .populate('user')
+         .populate({
+               path:'comments',
+               populate:{
+               path:'user',
+               }
+          });
+
+        let users = await User.find({});
+    
+        return res.render('home',{
+        title: "Codeella | Home",
+        posts:posts,
+        all_users:users
         });
-        
-    })
+
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
+   
+    
+          
+
+   
 }
 
 
