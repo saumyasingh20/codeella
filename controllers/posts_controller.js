cd01efed4ee460c4bc7628179a1f137b7b0f19ff
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-02-01 23:27:29 +0530

    converted create and deleting posts request to ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,10 +3,18 @@ const Post = require('../models/post');
 const Comment = require('../models/comment');
 module.exports.create = async function(req,res){
     try{
-        await Post.create({
+        let post =  await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post:post
+                },
+                message:'Post created !'
+            })
+        }
         req.flash('success','Post Published !');
         return res.redirect('back');
     }catch(err){
@@ -25,7 +33,19 @@ module.exports.destroy = async function(req,res){
             post.remove();  
                 
             await Comment.deleteMany({post:req.params.id});
-            req.flash('success','Post Deleted !');
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id:req.params.id
+                    },
+                    message:"Post deleted"
+                });
+            }
+
+
+
+            req.flash('success','Post and associated comments Deleted !');
             return res.redirect('back');
         }
         else {
