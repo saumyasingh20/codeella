e7a712bc3400c709292e7cae3192a64581de2dc4
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-02-05 18:52:50 +0530

    created users api to sign in and generated jwt token

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,36 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+module.exports.createSession = async function(req,res){
+    try{
+        let user = await User.findOne({email: req.body.email});
+
+        if(!user || user.password != req.body.password){
+            return res.status(422).json({
+                message:"Invalid username and password"
+            });
+        }
+        return res.status(200).json({
+            message:"Sign in successful, here is your token, please keep it safe",
+            data:{
+                token: jwt.sign(user.toJSON(),'xxxblehxx',{expiresIn:'10000'})
+            }
+        });
+
+
+    }catch(err){
+        console.log('******',err);
+        return res.status(500).json({
+            message:"internal server error"
+        });
+    }
+  
+
+
+
+
+
+
+
+
+
+}
