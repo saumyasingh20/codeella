b7a841c9a4b2cc27a5864a7dc3ee21164f6cde7d
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-02-11 23:21:20 +0530

    added like schema to db

---
+++ models/like.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:User,
+        required:true
+    },
+    //likeable is the object on which like has been placed like or comment
+    //it defines the object id of the liked object
+    likeable:{
+        type:mongoose.Schema.ObjectId,
+        refPath:'onModel',
+        required:true
+    },
+    //used for defining the type of the liked object since this is a dynamic reference
+    onModel:{
+        type:String,
+        required:true,
+        enum:['Post','Comment']
+    }
+},{
+    timestamps:true
+}
+);
+
+const Like = mongoose.model('Like',likeSchema);
+module.exports = Like;
